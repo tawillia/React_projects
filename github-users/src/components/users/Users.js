dd0fe5e7e56bf8cb7c6713465eaa357c2d226515
Tatiana <tbw32@hotmail.com>
2019-12-24 14:02:41 -0500

    Refactoring to Hooks and Context
    Search Class to Function with useState Hook
    User class to Function with useEffect Hook
    Implementing Context

--- github-users/src/components/users/Users.js
+++ github-users/src/components/users/Users.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 // import React, { Component } from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const Users = ({ users, loading }) => {
+const Users = () => {
+	const githubContext = useContext(GithubContext);
+	const { loading, users } = githubContext;
+
 	if (loading) {
 		return <Spinner />;
 	} else {
@@ -18,44 +21,6 @@ const Users = ({ users, loading }) => {
 			</div>
 		);
 	}
-	// class Users extends Component {
-	// state = {
-	// 	users: [
-	// 		{
-	// 			id: '1',
-	// 			ligin: 'mojombo',
-	// 			avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-	// 			html_url: 'https://github.com/mojombo'
-	// 		},
-	// 		{
-	// 			id: '2',
-	// 			ligin: 'defunkt',
-	// 			avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-	// 			html_url: 'https://github.com/defunkt'
-	// 		},
-	// 		{
-	// 			id: '3',
-	// 			ligin: 'pjhyett',
-	// 			avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-	// 			html_url: 'https://github.com/pjhyett'
-	// 		}
-	// 	]
-	// };
-
-	// return (
-	// 	<div style={userStyle}>
-	// 		{users.map(user => (
-	// 			// {this.props.users.map(user => (
-	// 			// {this.state.users.map(user => (
-	// 			<UserItem key={user.id} user={user} />
-	// 		))}
-	// 	</div>
-	// );
-};
-
-Users.prooTypes = {
-	users: PropTypes.array.isRequired,
-	loading: PropTypes.bool.isRequired
 };
 
 const userStyle = {
