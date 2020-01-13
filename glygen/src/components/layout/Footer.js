048c597d3db7abcb566fe02ca0bed581b6603c6c
Tatiana <tbw32@hotmail.com>
2020-01-12 22:41:42 -0500

    Updated footer

--- glygen/src/components/layout/Footer.js
+++ glygen/src/components/layout/Footer.js
@@ -5,14 +5,12 @@ import logoFooter from '../images/glygen_logos/glygen-logoW-top.svg';
 import uga from '../images/univ_logos/logo-uga.png';
 import gwu from '../images/univ_logos/logo-gwu.png';
 import { Navbar, Col, Nav, Image, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import '../../App.css';
 
 const Footer = () => (
 	<React.Fragment className='fixed-bottom'>
-		<Navbar
-			sticky='bottom'
-			className='gg-bg-blue text-center footer'
-			expand='lg'>
+		<Navbar sticky='bottom' className='gg-bg-blue text-center footer'>
 			<Container>
 				<Row>
 					<Col sm={12} md={6} xl={2}>
@@ -21,7 +19,7 @@ const Footer = () => (
 								href='#home'
 								src={logoFooter}
 								alt='Glygen'
-								className='d-inline-block align-top text-center'
+								className='justify-content-center'
 							/>
 						</Navbar.Brand>
 					</Col>
@@ -42,35 +40,33 @@ const Footer = () => (
 						</Nav>
 					</Col>
 					<Col sm={12} md={6} xl={4}>
-						<Nav inline className='justify-content-center'>
-							Funded by
-							<span>
-								<Nav.Item>
-									<Nav.Link
-										href='https://commonfund.nih.gov/glycoscience'
-										target='_blank'>
-										NIH Glycoscience Common Fund
-									</Nav.Link>
-								</Nav.Item>
-							</span>
-						</Nav>
-
-						<Nav inline className='justify-content-center'>
-							Grant #
-							<span>
-								<Nav.Item>
-									<Nav.Link
-										href='https://projectreporter.nih.gov/project_info_details.cfm?aid=9391499&icde=0'
-										target='_blank'>
-										1U01GM125267&nbsp;-&nbsp;01
-									</Nav.Link>
-								</Nav.Item>
-							</span>
-						</Nav>
+						<Navbar.Text>
+							Funded by{' '}
+							<a
+								href='https://commonfund.nih.gov/glycoscience'
+								target='_blank'
+								rel='noopener noreferrer'>
+								NIH Glycoscience Common Fund
+							</a>
+						</Navbar.Text>
+						<br />
+						<Navbar.Text>
+							Grant #{' '}
+							<a
+								href='https://projectreporter.nih.gov/project_info_details.cfm?aid=9391499&icde=0'
+								target='_blank'
+								rel='noopener noreferrer'>
+								1U01GM125267&nbsp;-&nbsp;01
+							</a>
+						</Navbar.Text>
 					</Col>
 					<Col sm={12} md={6} xl={2}>
-						<Image src={uga} />
-						<Image src={gwu} />
+						<Link to='https://www.ccrc.uga.edu/'>
+							<Image src={uga} className='univ-logo' />
+						</Link>
+						<a href='https://smhs.gwu.edu/'>
+							<Image src={gwu} className='univ-logo' />
+						</a>
 					</Col>
 				</Row>
 			</Container>
